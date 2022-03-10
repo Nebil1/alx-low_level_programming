7366d6a7debe1bacda11cd58bb9340fbd8672f28
Nebil1 <nebilyk@gmail.com>
2022-03-10 10:08:48 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * Description: main - prints out a string to stdout
+ * Return: 0 if sucess 
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
