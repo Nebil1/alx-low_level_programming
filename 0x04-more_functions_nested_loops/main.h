29497d25075d3aa2cb90233cf1239311847f4b9a
Nebil1 <nebilyk@gmail.com>
2022-03-17 20:49:03 +0300

    main

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,43 +1,33 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <unistd.h>
+#ifndef INV_HOLBERTON_H
+#define INV_HOLBERTON_H
 
 int _putchar(char c);
 
-/* 0 */
 int _isupper(int c);
 
-/* 1 */
 int _isdigit(int c);
 
-/* 2 */
 int mul(int a, int b);
 
-/* 3 */
 void print_numbers(void);
 
-/* 4 */
 void print_most_numbers(void);
 
-/* 5 */
 void more_numbers(void);
 
-/* 6 */
 void print_line(int n);
 
-/* 7 */
 void print_diagonal(int n);
 
-/* 8 */
 void print_square(int size);
 
-/* 10 */
 void print_triangle(int size);
 
-/* 11 */
+int isPrime(long int n);
 
-/* 12 */
+long int biggestFactor(long int a);
+
 void print_number(int n);
 
-#endif /* MAIN_H */
+int numDigits(int n);
+#endif
