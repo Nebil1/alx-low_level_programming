0725fa87496fa0f4038008ae105b690b1e6d5e3f
Nebil1 <nebilyk@gmail.com>
2022-03-24 20:15:18 +0300

    a function that adds two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,61 +1,33 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_line - prints a s bytes of a buffer
- * @c: buffer to print
- * @s: bytes of buffer to print
- * @l: line of buffer to print
+ * infinite_add - function with 4 arguments
+ * @n1: char type pointer argument
+ * @n2: char type pointer argument
+ * @r: char type pointer argument
+ * @size_r: int type argument
  *
- * Return: void
+ * Description: adds two numbers from string
+ * Return: sum of two integers
  */
-
-void print_line(char *c, int s, int l)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int j, k;
+	int count, count2;
 
-	for (j = 0; j <= 9; j++)
-	{
-		if (j <= s)
-			printf("%02x", c[l * 10 + j]);
-		else
-			printf("  ");
-		if (j % 2)
-			putchar(' ');
-	}
-	for (k = 0; k <= s; k++)
-	{
-		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
-			putchar(c[l * 10 + k]);
-		else
-			putchar('.');
-	}
-}
+	while (n1[count] != '\0')
+		count++;
+	while (n2[count2] != '\0')
+		count2++;
 
-/**
- * print_buffer - prints a buffer
- * @b: buffer to print
- * @size: size of buffer
- *
- * Return: void
- */
-void print_buffer(char *b, int size)
-{
-	int i;
-
-	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	*r = *(r + size_r);
+	while (n1[count] > 0 || n1[count2] > 0)
 	{
-		printf("%08x: ", i * 10);
-		if (i < size / 10)
-		{
-			print_line(b, 9, i);
-		}
+		if (n1[count] + n2[count2] > 0)
+			*r = n1[count - 1] + n2[count2 - 1] + 1;
 		else
-		{
-			print_line(b, size % 10 - 1, i);
-		}
-		putchar('\n');
+			*r = n1[count] + n2[count2];
+		count--;
+		count2--;
 	}
-	if (size == 0)
-		putchar('\n');
+	return (r);
 }
