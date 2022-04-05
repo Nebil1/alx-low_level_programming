4b1a95a2821dc1524e18acc7ee830b9ddca9e138
Nebil1 <nebilyk@gmail.com>
2022-04-05 15:59:29 +0300

    a function that splits a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,97 +1,43 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
+
 /**
- * words - description
- *@str: description
- *Return: description
- */
-int words(char *str)
-{
-int i = 0, l = 0;
-while (str[i] != '\0')
-{
-if (str[i] == ' ')
-i++;
-else
-{
-l++;
-while (str[i] != ' ')
-i++;
-}
-}
-return (l);
-}
-/**
- * display - description
- *@str: description
- *@s: description
- *@i: description
- *Return: description
- */
-int display(char *str, char *s, int i)
-{
-int z = 0;
-while (str[i] != ' ')
-{
-s[z] = str[i];
-i++;
-z++;
-}
-s[z] = '\0';
-return (i);
-}
-/**
- * letters - description
- *@x: description
- *@l: description
- *Return: description
- */
-int letters(char *x, int l)
-{
-int a = 0;
-while (x[l] != ' ')
-{
-a++;
-l++;
-}
-return (a);
-}
-/**
- * strtow - description
- *@str: description
- *Return: description
- */
+* **strtow - function that splits a string into words.
+* @str : pointer
+* Return: Char
+*/
+
 char **strtow(char *str)
 {
-int a = 0, l, i = 0, j, y;
-char **s = NULL;
-if (str == NULL || *str == '\0')
-return (NULL);
-l = words(str);
-if (l == 0)
-return (NULL);
-s = malloc(sizeof(char *) * l + 1);
-if (s == NULL)
-return (NULL);
-while (str[i] != '\0')
-{
-if (str[i] == ' ')
-i++;
-else
-{
-y = letters(str, i);
-s[a] = malloc(sizeof(char) * y + 1);
-if (s[a] == NULL)
-{
-for (j = 0 ; j <= a ; j++)
-free(s[j]);
-free(s);
-return (NULL);
-}
-i = display(str, s[a], i);
-a++;
-}
-}
-s[a] = NULL;
-return (s);
+	char **d;
+	int i;
+	int j = 0;
+	int con = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (str[i] != 32)
+			con++;
+	}
+
+	d = malloc(sizeof(char) * con);
+
+	if (d == NULL)
+		return (NULL);
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (str[i] != 32)
+		{
+			*d[j] = str[i];
+			j++;
+		}
+		else
+		{
+		}
+	}
+	return (d);
 }
